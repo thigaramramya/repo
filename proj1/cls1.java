41e4326970367bec889abdcbf9baacb608cee15d
thigaramramya <thigaram.ramya@gmail.com>
2021-03-01 00:45:29 +0530

    commit changes added in cls1

--- proj1/cls1.java
+++ proj1/cls1.java
@@ -3,6 +3,7 @@ package proj1;
 public class cls1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome to my project...");
+		System.out.println("Check the commit changes in Github...");
 	}
 
 }
